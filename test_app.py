ae15d95ad176f91f19d284be94bc80a76d3508e4
Chris Hughes <cahughes95@gmail.com>
2024-04-18 16:41:58 -0700

    updated workflow

--- test_app.py
+++ test_app.py
@@ -1,5 +1,6 @@
 from app import app
-with app.test_client() as c:
-    response = c.get('/')
-    assert response.data == b'Hello Everyone!'
-    assert response.status_code == 200
+def test_app_root_url():
+    with app.test_client() as c:
+        response = c.get('/')
+        assert response.data == b'Hello Everyone!'
+        assert response.status_code == 200
